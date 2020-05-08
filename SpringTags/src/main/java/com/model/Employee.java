8ae45d63a202e9682ab9a88ef9f40ffa7e675e0e
jatinbansalprogrammer <jatinbansalprogrammer@gmail.com>
2020-05-09 05:13:15 +0530

    Revert "a"
    This reverts commit 04a420beb30a1089637a7f881c39d3e0d394d13d.

--- SpringTags/src/main/java/com/model/Employee.java
+++ SpringTags/src/main/java/com/model/Employee.java
@@ -3,41 +3,35 @@ package com.model;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+
+
+
 public class Employee {
 	
-	private int id;
-	private String name;
-	private String statename;
-	private Address address;
+	@NotNull(message = "is Required")
+	@Size(min = 2, message = "enter minimum 1 char...." )
+	private String empName;
+	private int empAge;
 	
 	public Employee() {
-		System.out.println("");
-	}
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getStatename() {
-		return statename;
-	}
-	public void setStatename(String statename) {
-		this.statename = statename;
-	}
-	public Address getAddress() {
-		return address;
-	}
-	public void setAddress(Address address) {
-		this.address = address;
+		System.out.println();
 	}
 	
 	
+	public String getEmpName() {
+		return empName;
+	}
+	
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	
+	public int getEmpAge() {
+		return empAge;
+	}
+	
+	public void setEmpAge(int empAge) {
+		this.empAge = empAge;
+	}
+
 }
